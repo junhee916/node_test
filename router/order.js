9a88787c19add358a9c84e555814864cb0393321
junhee916 <junhee916@naver.com>
2021-04-16 21:36:04 +0900

    body-parser order started

--- router/order.js
+++ router/order.js
@@ -2,14 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+
     res.json({
-        massege : "get order"
+        massege : "get order",
     })
 })
 
 router.post('/', (req, res) => {
+    const newOrder = {
+        product : req.body.productId,
+        quentity : req.body.qty
+    }
     res.json({
-        message : "resgister order"
+        message : "resgister order",
+        orderInfo : newOrder
     })
 })
 
